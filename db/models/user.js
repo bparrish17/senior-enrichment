581cd4ca63de4418135ed4b3b3ce1e9f2a247878
Brian Parrish <brian.parrish3739@gmail.com>
2017-08-24 18:03:22 -0400

    Added some routes

--- db/models/user.js
+++ db/models/user.js
@@ -2,7 +2,26 @@
 var Sequelize = require('sequelize')
 var db = require('../index.js')
 
-
-module.exports = db.define('user', {
-  name: Sequelize.STRING,
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: Sequelize.STRING,
+  campus: Sequelize.STRING
 })
+
+const Campus = db.define('campus', {
+  name: Sequelize.STRING,
+  imgURL: Sequelize.TEXT,
+  students: Sequelize.ARRAY(Sequelize.STRING) 
+})
+
+
+User.belongsTo(Campus, { as: 'student'})
+
+module.exports = {
+  User: User, 
+  Campus: Campus
+}
+
